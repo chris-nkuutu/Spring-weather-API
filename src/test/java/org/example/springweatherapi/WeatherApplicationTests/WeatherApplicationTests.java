d26f5d5e96814dbe0e45a28a2a908b0926981495
Chris_nkuutu <nkuutugeorge@gmail.com>
2024-01-22 22:06:41 +0100

    Update WeatherApplicationTests.java

--- src/test/java/org/example/springweatherapi/WeatherApplicationTests/WeatherApplicationTests.java
+++ src/test/java/org/example/springweatherapi/WeatherApplicationTests/WeatherApplicationTests.java
@@ -1,8 +1,12 @@
 package org.example.springweatherapi.WeatherApplicationTests;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ImportResource;
 
+@SpringBootApplication
+@ImportResource({"classpath*:application-context.xml"})
 @SpringBootTest
 class WeatherApplicationTests {
 
